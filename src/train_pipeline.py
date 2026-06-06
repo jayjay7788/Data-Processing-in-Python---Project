b6a562a050622c9bc42efdbc3be3fd218248bc27
Markéta Ondřejová <27856055@fsv.cuni.cz>
2026-06-06 20:30:35 +0200

    added comments

--- src/train_pipeline.py
+++ src/train_pipeline.py
@@ -3,28 +3,30 @@ import sys
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
-# project root is to the system path to allow importing wrapper
+# project root 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
-# Import model wrapper functions
+# import model wrapper functions
 from src.model_wrapper import save_model_bundle
 
+# the training function
 def run_training_pipeline():
-    # Define paths based on your structure
+    # define paths based on the structure
     processed_data_path = PROJECT_ROOT / "data" / "processed" / "processed_data.csv"
     model_output_dir = PROJECT_ROOT / "results" / "models"
     
+    # print process info and error message
     print(f"Loading processed data from: {processed_data_path}")
     if not processed_data_path.exists():
         raise FileNotFoundError(
             f"Could not find processed data. Please run your preprocessing script first!"
         )
-        
+    # read the data    
     df = pd.read_csv(processed_data_path)
     
-    # Define features that match Streamlit App Sliders exactly
+    # define features that will be used in the model
     numeric_features = [
         "hour", "dayofweek", "is_weekend", "month", "dayofyear",
         "weather_T", "weather_H", "weather_P", "weather_F", "wind_u", "wind_v",
@@ -37,42 +39,43 @@ def run_training_pipeline():
     ]
     targets = ["air_NO2", "air_NOx", "air_PM10"]
     
-    # Clean up any missing rows in our training target features
+    # clean up any missing rows in the training target features
     df_clean = df.dropna(subset=numeric_features + targets)
     X = df_clean[numeric_features]
     print("Sample station names in training data:", df["air_station_name"].unique())
     trained_models = {}
     
-    # Train a distinct model for each air pollutant
+    # train a distinct model for each air pollutant
     for target in targets:
         print(f" Training Random Forest Regressor for {target}...")
         y = df_clean[target]
         
-        # Using a fast configuration; adjust hyperparameters as needed
+        # fit the RF model
         model = RandomForestRegressor(n_estimators=100, random_state=42, n_jobs=-1)
         model.fit(X, y)
         
         trained_models[target] = model
     
-    # Use model wrapper to save the bundle precisely where the app expects it
+    # use the predefined structure from model_wrapper and app_draft to save the model outputs
     print(f" Saving trained models into bundle...")
     bundle_path = save_model_bundle(
         models=trained_models,
         save_path=model_output_dir,
         numeric_features=numeric_features
     )
-    print(f"🎉 Success! Model bundle successfully exported to: {bundle_path}")
+    print(f"Model bundle successfully exported to: {bundle_path}")
 
 if __name__ == "__main__":
     run_training_pipeline()
 
 
 
-
+# the prediction function
 def predict_pollution(w_speed, temp, hum, pressure, rain, stat):
+    # link it to the bundle
     bundle = get_model_bundle()
     
-    # Calculate time features to feed the model
+    # calculate time features to feed the model
     now = pd.Timestamp.now()
     hour = now.hour
     dayofweek = now.dayofweek
@@ -80,7 +83,7 @@ def predict_pollution(w_speed, temp, hum, pressure, rain, stat):
     month = now.month
     dayofyear = now.dayofyear
 
-    # Map the UI sliders to the EXACT column names your models were trained on
+    # map the UI sliders to the column names the models were trained on
     input_data = pd.DataFrame([
         {
             "weather_T": temp,
